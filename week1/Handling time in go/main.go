1ce0fd406dcd136db31168e6bd72db84076fedea
unknown <btech10464.23@bitmesra.ac.in>
2026-06-20 01:54:16 +0530

    adding time

---
+++ week1/Handling time in go/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	fmt.Println("Welcome to the time study of golang")
+
+	presentTime := time.Now()
+	fmt.Println(presentTime)
+
+	fmt.Println(presentTime.Format("01-02-2006 15:04:05 Monday"))
+
+	createDate := time.Date(2020, time.June,10, 23, 25, 0, 0, time.UTC)
+	fmt.Println(createDate)
+	fmt.Println(createDate.Format("01-02-2006 Monday"))
+
+}
