3dbb42cacdd43bfb6cf93f0a78e710ad94109787
unknown <btech10464.23@bitmesra.ac.in>
2026-07-11 03:01:56 +0530

    url

---
+++ week3/myurls/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"net/url"
+)
+
+const myurl string = "https://lco.dev:3000/learn?coursename=reactjs&paymentid=ghbj456ghb"
+
+func main() {
+	fmt.Println("Welcome to handling URLs in Golang")
+	fmt.Println(myurl)
+
+	//parsing
+	result, _ := url.Parse(myurl)
+
+	fmt.Println(result.Scheme)
+	fmt.Println(result.Host)
+	fmt.Println(result.Path)
+	fmt.Println(result.RawQuery)
+
+}
