18799d0e3c5224a7003b7ab0005e0598c73d8cca
unknown <btech10464.23@bitmesra.ac.in>
2026-07-20 09:57:53 +0530

    funwithjson

---
+++ week3/Bitmorejson/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type course struct {
+	Nmae     string `json:"coursename"`
+	Price    int
+	Platform string `json:"website"`
+	Password string `json:"-"`
+	Tags     []string `json:"tags,omitempty"`
+}
+
+func main() {
+	fmt.Println("Welcome to Bitmorejson")
+	EncodeJson()
+
+}
+
+func EncodeJson() {
+
+	lcoCourses := []course{
+		{"ReactJS Bootcamp", 299, "LearnDev.online.in", "abc12345", []string{"Web-dev", "js"}},
+		{"MERN Bootcamp", 599, "LearnDevonline.in", "bcd12345", []string{"Fullstack", "js", "mern"}},
+		{"Angular Bootcamp", 299, "LearnDevonline.in", "cde12345", nil},
+	}
+
+	//Package this data as Json Data
+
+	finalJson, err := json.MarshalIndent(lcoCourses, "", "\t")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%s\n", &finalJson)
+
+}
